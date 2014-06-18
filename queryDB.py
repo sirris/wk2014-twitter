251a66cb5640ecfbf4e30d0251bebd165d12daac
Tom Ruette <tom.ruette@sirris.be>
2014-06-18 19:47:05 +0000

    a rough approach to geotweets is in place

--- queryDB.py
+++ queryDB.py
@@ -41,6 +41,41 @@ def tweetsPerLevel(c, variable):
     out.append( str(item['tweetsPerLevel']) + '\t' + unicode(item['_id']) )
   return '\n'.join(out)
 
+def geotweets(c):
+  """ get a geojson list of points where tweets were tweeted in BE"""
+  lst = []
+  for tweet in c.find({'place': {'$ne': 'null'}}):
+    try:
+      place = tweet['place']
+      if place['country_code'] == 'BE':
+        point = findCenter(place['bounding_box']['coordinates'][0])
+        geojson = {"type": "Feature",
+                   "properties": 
+                     {
+                      "name": str(tweet['user']['screen_name']).strip(),
+                      "amenity": "Tweet",
+                      "popupContent": str(tweet['user']['screen_name']).strip()
+                     },
+                  "geometry": 
+                    {
+                      "type": "Point",
+                      "coordinates": [point[0], point[1]]
+                    }
+                  }
+        lst.append(geojson)
+    except:
+      continue
+  return lst
+
+def findCenter(coordinates):
+  """ simple averaging approach to find the center of a polygon, awful approach """
+  longs = []
+  lats = []
+  for coordinate in coordinates:
+    longs.append(coordinate[0])
+    lats.append(coordinate[1])
+  return [sum(longs)/float(len(coordinates)), sum(lats)/float(len(coordinates))]
+
 def write(s, fname):
   fout = codecs.open(fname, 'w', 'utf-8')
   fout.write( unicode(s) )
@@ -59,7 +94,8 @@ def main():
   # start querying
 #  write( totalNumberOfTweets(mongocollection), './tweets.tab' )
 #  write( totalNumberOfUsers(mongocollection), './users.tab' )
-  write( tweetsPerLevel(mongocollection, 'user.screen_name'), 'users.freq.tab' )
+#  write( tweetsPerLevel(mongocollection, 'user.screen_name'), './users.freq.tab' )
+  write( geotweets(mongocollection), './locations.geojson' )
 
 if __name__ == '__main__':
   main()
