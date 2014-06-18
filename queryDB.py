ed4a97f6f742849e110faa5358be0b96c5d852e2
Tom Ruette <tom.ruette@sirris.be>
2014-06-18 18:08:44 +0000

    initialized the query script

---
+++ queryDB.py
@@ -0,0 +1,6 @@
+###############################################################################
+# queryDB.py provides functions for querying the local mongoDB that contains 
+# the Twitter messages that were collected.
+###############################################################################
+
+
